7dbd5c14899a1aaa0be3a0fce5028d660a876dbc
Andrew Bloomgarden <stalkingtiger@gmail.com>
2010-06-01 00:23:19 -0700

    Upgrade RSpec.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,12 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-require 'resource_defaults'
-require 'spec'
-require 'spec/autorun'
 
-Spec::Runner.configure do |config|
-  
-end
+require 'rubygems'
+# Set up gems listed in the Gemfile.
+require 'bundler'
+Bundler.setup
+Bundler.require
+
+require 'resource_defaults'
+require 'rspec'
+require 'rspec/autorun'
